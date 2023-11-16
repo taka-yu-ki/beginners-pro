c0706ac39dcdbf806c43cc237e31b93e2d4cb197
taka-yu-ki <taka7_zegs@icloud.com>
2023-11-16 14:37:34 +0900

    マイグレーションファイルの作成・実行

---
+++ database/migrations/2023_11_16_052544_create_note_record_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note_record_comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('note_record_id')->constrained();
+            $table->string('comment', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note_record_comments');
+    }
+};
