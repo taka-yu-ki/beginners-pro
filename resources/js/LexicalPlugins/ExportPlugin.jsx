5c3517b6432cf1ca362ee13ab3107ca41448ec67
taka-yu-ki <taka7_zegs@icloud.com>
2024-02-06 23:04:00 +0900

    lexicalの未入力状態のカスタム

--- resources/js/LexicalPlugins/ExportPlugin.jsx
+++ resources/js/LexicalPlugins/ExportPlugin.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { $isRootTextContentEmpty } from "@lexical/text";
 
 export default function ExportPlugin({onChange}) {
     const [editor] = useLexicalComposerContext();
-    
+
     useEffect(() => {
         if(onChange) {
             editor.registerUpdateListener(({editorState}) => {
                 editorState.read(() => {
-                    const contentAsJSON = JSON.stringify(editorState);
-                    onChange(contentAsJSON);
+                    const isEmpty = $isRootTextContentEmpty(editor.isComposing());
+                    if (isEmpty) {
+                        onChange("");
+                    } else {
+                        const contentAsJSON = JSON.stringify(editorState);
+                        onChange(contentAsJSON);
+                    }
                 });
             });
         }
