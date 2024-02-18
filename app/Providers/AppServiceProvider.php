6de8c5ca7e324fe1196cb8705513f89f0499851a
taka-yu-ki <taka7_zegs@icloud.com>
2024-02-18 13:21:32 +0900

    update package.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
-        $this->app['request']->server->set('HTTPS','on');
+        if (config('app.env') !== 'local') {
+            \URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS','on');
+        }
     }
 }
