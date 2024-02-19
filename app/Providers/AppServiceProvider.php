cdde248cc80c3545868ccf59c591bf44fa0126cc
taka-yu-ki <taka7_zegs@icloud.com>
2024-02-19 15:17:25 +0900

    https設定の変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment(['production'])) {
-            \URL::forceScheme('https');
-            $this->app['request']->server->set('HTTPS','on');
-        }
+        \URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS','on');
     }
 }
